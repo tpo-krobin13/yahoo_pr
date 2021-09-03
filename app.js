b6fd6c1694e99c9c67682ba96cdf1a51fccd8dc1
Ken Robinson <krobinson@kodehaus.com>
2021-09-03 01:16:52 -0400

    removed comments

--- app.js
+++ app.js
@@ -5,10 +5,11 @@ const data = require('./data');
 var Cookies = require('cookies');
 var dt = require('date-and-time');
 var cookieKeys = ['bfl fo life'];
+const path = require('path');
 
 const httpsOptions = {
-  key: fs.readFileSync('/Users/krobinson/key.pem'),
-  cert: fs.readFileSync('/Users/krobinson/cert.pem')
+  key: fs.readFileSync(path.join(process.cwd(), './key.pem')),
+  cert: fs.readFileSync(path.join(process.cwd(), './cert.pem'))
 }
 
 
@@ -29,8 +30,7 @@ app.engine('html', require('ejs').renderFile);
 
 
 function refreshToken(paramA, paramB){
-  console.log('loggin the refresh token: ')
-  console.log(paramB);
+//  console.log(paramB);
 }
 
 const YahooFantasy = require('yahoo-fantasy');
@@ -55,6 +55,7 @@ function transferCredentials(responseObj){
 }
 
 
+
 app.get('/', (req, res) => {
   res.render('index.html' );
 });
@@ -67,8 +68,9 @@ app.get('/authRedirect', (req, res) => {
   yf.authCallback(
     req, refreshToken
   )
-  data.yahooFSResource()
-  yf.league.
+  console.log(yf);
+  res.redirect('/');
+  res.end();
 })
 
 
@@ -91,8 +93,6 @@ app.use('*',(req, res, next) => {
   app.use((err, req, res, next) => {
       res.locals.error = err;
       res.status(err.status);
-//      console.error(req.url);
-//      console.dir(err);
       if (err.status == 404) {
         res.render('page-not-found');
       } else {
@@ -112,27 +112,4 @@ httpServer.listen(httpPort, () => {
 
 https.createServer(httpsOptions, app).listen(httpsPort, () => {
     console.log(`The https server is running on port: ${httpsPort}`);
-});
-
-
-
-app.get('/Delete---------/auth', (req, res) => {
-  const url = `${config.yahoo.auth_url}request_auth?client_id=${config.yahoo.app_key}&redirect_uri=${config.yahoo.redirect_uri}&response_type=code`;
-  res.redirect(url);
-});
-app.get('/Delete---------authRedirect', async (req, res) => {
-  const authToken = req.query.code;
-  
-  const response = await data.getAccessToken(authToken);
-  const accessObj = {};
-  accessObj.access_token = 'RvP9Y9Kfug0cIKb1HZpGASDvaIuuWEttO0yRn4NkIMaSKop32Ksfnp7an6ennNFmwM0tcf2gaDRuoiHUizkakPhfWXwndO8X8iY.fbEBdocsqADP8sROFg1IYlw7pYwGZYJosR33qEhomSL9_eXfZT8dAfKWcsNyteY_gYyHdPOlqboWChwZD2mqM24hus2WU4ne.3Sxy4uv2zVWc21yOgfNZdBLEruBXrdAee_FYPdmLwoUXIbtOgXEWH6SOhtas06wExrvwnpfGXVldv7esDBvTFha2xXhOejnogalzY7XzoKuEaWs3VHPVX2ueuCEZ4H3wg_IJYeFD6pYRgwcQozRQAB_OUuzwnWD4vba04TABcfqR6EEbIfyqC1eg0ZNuYVz4EYpiISk01O8P_h.kKYEW6aa.xyvcMK45Ug0yzq2KTSDJV9u3wob17A8vs8H3DLyFUOGq6U_SpQk38.koCEubSVBNpSbBcJBYWJ_oRF1XoEuQyr5FCMszesOxkeH1HYGPHs0GhUXqhIb6Z8MQNrkpUZdky_Vk87vSp6vqiuQnDM1bxe.q.e2jwAunxQBZUeCfVePm_VYQTZJqQtqFhm4NVZ3X2EimOZZH40Y9jjNTkzrUBxQnSBRFBBARVGdViKZ2mi1u2Hqb4hGH1e8ngbhtoXC7uIXRwW.NQ3uPbc0dgNq0Cls16ApIKQdl8BZAB4M8w6Xgls_2YOBoL0TcwtSdHnosmBEh9y7RHoP2Z5j18KCITUkuJTky2.xvqJyL11Q9gRJhN0LG1Jhwax7Rkksqh7nH7Vy3atZ4lSsXiBEcFVhEMOlvQWA3Op5Ea90O3EpG3BcaHARjUunEQOvrJPHSkZvxEwDgOJmaiHbhBvH1jh.zHFAIuX6gzcbM7mDU2nNdt1.V5LjpEo1tAhKhJWYhID8wvVLiuiFnj1CHcwUoDEvQ7P_cBtnAqn6sywhzuQCvmIcIL1ESqQkkxM1Izff13JP4KacOIbqCu7eFw5jIY5L.8YptmFG0w--';
-  accessObj.refresh_token = 'ADrOlWDkJmqHR_ybNvdQVwUgMGd5J1qKYqWL2GkWbAbZV3TqhBS5';
-  accessObj.expires_in = 3600;
-  accessObj.token_type = 'bearer';
-  Cookies.set('authenticatedUser', JSON.stringify(accessObj), {expires: 1});
-
-  const accessObjedt = response.data; 
-  console.log('my access element -----------------------------')
-
-  res.end();
 });
