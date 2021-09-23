5a3ed1edcb57110c7b3ce381aa202b542c437356
Ken Robinson <krobinson@kodehaus.com>
2021-09-22 20:11:56 -0400

    trying this

--- app.js
+++ app.js
@@ -2,48 +2,44 @@ var fs = require('fs');
 var http = require('http');
 var https = require ('https');
 const data = require('./data');
-var Cookies = require('cookies');  
-let cookieKeys = ['BFL Fo Life'];
-
-
+var Cookies = require('cookies');
 var dt = require('date-and-time');
-const path = require('path');
-const cfg = require('./config/');
+var cookieKeys = ['bfl fo life'];
 
 const httpsOptions = {
-  key: fs.readFileSync(path.join(process.cwd(), cfg.sslKey)),
-  cert: fs.readFileSync(path.join(process.cwd(), cfg.sslCert))
+  key: fs.readFileSync('/Users/krobinson/key.pem'),
+  cert: fs.readFileSync('/Users/krobinson/cert.pem')
 }
 
 
 const express = require('express');
 const bodyParser = require('body-parser');
 const cookieParser = require('cookie-parser');
+const config = require('./config');
 
 const app = express();
 
 
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(cookieParser());
-app.use(express.static('public'))
-//TODO refactor this file to serve from the styles folder
-app.use("/style.css", express.static('/public/css/style.css')) 
+app.use('/static',express.static('public'));
 app.set('view engine', 'ejs');
 
 app.engine('html', require('ejs').renderFile);
 
 
 function refreshToken(paramA, paramB){
-
+  console.log('loggin the refresh token: ')
+  console.log(paramB);
 }
 
 const YahooFantasy = require('yahoo-fantasy');
 
 const yf = new YahooFantasy(
-  cfg.yahooAppKey,
-  cfg.yahooAppSecret,
+  config.yahoo.app_key,
+  config.yahoo.app_secret,
   refreshToken,
-  `https://${cfg.domain}:${cfg.sslPort}/${cfg.yahooRedirectRoute}`
+  'https://127.0.0.1:4000/authRedirect/'
 )
 
 
@@ -59,41 +55,20 @@ function transferCredentials(responseObj){
 }
 
 
-
-app.get('/', async (req, res) => {
-  try {
-    var cookies = new Cookies(req, res, { keys: cookieKeys })
-    var token = cookies.get('refreshToken', { signed: true })
-    if(token){
-      console.log('token is true');
-      console.log('token:' + token);
-    } else {
-      console.log('token:' + token);
-      console.log('token is false');
-    }
-    res.render('returning.html' );
-
-    
-  } catch (e) {
-    console.log(e);
-    res.render('500.html' );
-  }
-  res.end();
+app.get('/', (req, res) => {
+  res.render('index.html' );
 });
 
 app.get('/authYahooUser', (req, res) => {
   yf.auth(res);
 })
 
-// yahoo will redirect the user to the local page once all authorization is completed
 app.get('/authRedirect', (req, res) => {
-  yf.authCallback(req, refreshToken)
-//  cookies.set('yf.userToken', yf.yahooUserToken, { signed: true })
-//  cookies.set('yf.refreshToken', yf.yahooRefreshToken, { signed: true })
-//  cookies.set('yf.tokenExp', yf.yahooRefreshToken, { signed: true, expires:3600000 + Date.now() })
-
-  res.redirect('/returning.html');
-  res.end();
+  yf.authCallback(
+    req, refreshToken
+  )
+  data.yahooFSResource()
+  yf.league.
 })
 
 
@@ -119,22 +94,45 @@ app.use('*',(req, res, next) => {
 //      console.error(req.url);
 //      console.dir(err);
       if (err.status == 404) {
-        res.render('404.html');
+        res.render('page-not-found');
       } else {
         err.status = 500;
-        res.render('500.html');
+        res.json(err);
       }
-      res.end();
   })
 
+var httpServer = http.createServer(app);
 
+const httpPort = 3000;
+const httpsPort = 4000;
 
-http.createServer(app).listen(cfg.port, () => {
-    console.log(`The http server is running on port: ${cfg.port}`);
+httpServer.listen(httpPort, () => {
+    console.log(`The http server is running on port: ${httpPort}`);
 });
 
-https.createServer(httpsOptions, app).listen(cfg.sslPort, () => {
-    console.log(`The https server is running on port: ${cfg.sslPort}`);
+https.createServer(httpsOptions, app).listen(httpsPort, () => {
+    console.log(`The https server is running on port: ${httpsPort}`);
 });
 
-exports.app = app;
+
+
+app.get('/Delete---------/auth', (req, res) => {
+  const url = `${config.yahoo.auth_url}request_auth?client_id=${config.yahoo.app_key}&redirect_uri=${config.yahoo.redirect_uri}&response_type=code`;
+  res.redirect(url);
+});
+app.get('/Delete---------authRedirect', async (req, res) => {
+  const authToken = req.query.code;
+  
+  const response = await data.getAccessToken(authToken);
+  const accessObj = {};
+  accessObj.access_token = 'RvP9Y9Kfug0cIKb1HZpGASDvaIuuWEttO0yRn4NkIMaSKop32Ksfnp7an6ennNFmwM0tcf2gaDRuoiHUizkakPhfWXwndO8X8iY.fbEBdocsqADP8sROFg1IYlw7pYwGZYJosR33qEhomSL9_eXfZT8dAfKWcsNyteY_gYyHdPOlqboWChwZD2mqM24hus2WU4ne.3Sxy4uv2zVWc21yOgfNZdBLEruBXrdAee_FYPdmLwoUXIbtOgXEWH6SOhtas06wExrvwnpfGXVldv7esDBvTFha2xXhOejnogalzY7XzoKuEaWs3VHPVX2ueuCEZ4H3wg_IJYeFD6pYRgwcQozRQAB_OUuzwnWD4vba04TABcfqR6EEbIfyqC1eg0ZNuYVz4EYpiISk01O8P_h.kKYEW6aa.xyvcMK45Ug0yzq2KTSDJV9u3wob17A8vs8H3DLyFUOGq6U_SpQk38.koCEubSVBNpSbBcJBYWJ_oRF1XoEuQyr5FCMszesOxkeH1HYGPHs0GhUXqhIb6Z8MQNrkpUZdky_Vk87vSp6vqiuQnDM1bxe.q.e2jwAunxQBZUeCfVePm_VYQTZJqQtqFhm4NVZ3X2EimOZZH40Y9jjNTkzrUBxQnSBRFBBARVGdViKZ2mi1u2Hqb4hGH1e8ngbhtoXC7uIXRwW.NQ3uPbc0dgNq0Cls16ApIKQdl8BZAB4M8w6Xgls_2YOBoL0TcwtSdHnosmBEh9y7RHoP2Z5j18KCITUkuJTky2.xvqJyL11Q9gRJhN0LG1Jhwax7Rkksqh7nH7Vy3atZ4lSsXiBEcFVhEMOlvQWA3Op5Ea90O3EpG3BcaHARjUunEQOvrJPHSkZvxEwDgOJmaiHbhBvH1jh.zHFAIuX6gzcbM7mDU2nNdt1.V5LjpEo1tAhKhJWYhID8wvVLiuiFnj1CHcwUoDEvQ7P_cBtnAqn6sywhzuQCvmIcIL1ESqQkkxM1Izff13JP4KacOIbqCu7eFw5jIY5L.8YptmFG0w--';
+  accessObj.refresh_token = 'ADrOlWDkJmqHR_ybNvdQVwUgMGd5J1qKYqWL2GkWbAbZV3TqhBS5';
+  accessObj.expires_in = 3600;
+  accessObj.token_type = 'bearer';
+  Cookies.set('authenticatedUser', JSON.stringify(accessObj), {expires: 1});
+
+  const accessObjedt = response.data; 
+  console.log('my access element -----------------------------')
+
+  res.end();
+});
